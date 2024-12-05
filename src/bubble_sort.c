8e6dbc1f262f9eec05f8276528067e9d2f8931e3
Athena Boose <pestpestthechicken@yahoo.com>
2024-12-04 16:21:57 -0800

    Added bubble sort, no unit tests.

--- src/bubble_sort.c
+++ src/bubble_sort.c
@@ -1,1 +1,19 @@
 #include "bubble_sort.h"
+
+void bubble_sort_int(int *array, size_t num_elements) {
+    bubble_sort(array, num_elements, sizeof(int), &int_cmp);
+}
+
+void bubble_sort(void *array, size_t num_elements, size_t width, int (*compare)(const void *, const void *)) {
+    void *temp = malloc(width);
+    for (size_t end = num_elements; end > 1; --end) {
+        for (size_t marker = 0; marker < end; ++marker) {
+            if (compare((char *)array + width * marker, (char *)array + width * (marker + 1)) > 0) {
+                memcpy(temp, (char *)array + width * marker, width); // temp = array[marker]
+                memcpy((char *)array + width * marker, (char *)array + width * (marker + 1), width); // array[marker] = array[marker + 1]
+                memcpy((char *)array + width * (marker + 1), temp, width); // array[marker + 1] = temp
+            }
+        }
+    }
+    free(temp);
+}
--- src/bubble_sort.h
+++ src/bubble_sort.h
@@ -2,8 +2,11 @@
 #define BUBBLE_SORT_H
 
 #include <stddef.h>
+#include <string.h>
 #include "compare.h"
 
-void bubble_sort(void *arrays, size_t num_elements, size_t width, int (*compare)(const void *, const void *));
+void bubble_sort_int(int *array, size_t num_elements);
+
+void bubble_sort(void *array, size_t num_elements, size_t width, int (*compare)(const void *, const void *));
 
 #endif
